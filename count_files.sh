f32db0efc83daea9d5a2f6ac0e85ca53ef092c30
Nadia495 <nutnad88@gmail.com>
2025-05-14 21:48:31 +0100

    count_files.sh

---
+++ count_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Verifier si un argument est fourni
+if [ -z "$1"]; then
+echo "Erreur: veuillez spécifier un repertoire"
+exit 1
+fi
+#Verifier si le répertoire existe
+if [!-d "$1"]; then
+echo "Erreur : le répertoire '$1' n'existe pas."
+exit 1
+fi
+#Compter le nombre de fichiers (exclut les dossiers cachés)
+nombre_fichuers=$(ls -lA "$1" | grep "^-" | wc -l)
+#Affiche le résultat 
+echo "le dossier $1 contient $nombre_fichiers fichier(s)."
